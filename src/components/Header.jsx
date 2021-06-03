490980109744e8d504cc20132a2092441eea8853
Gowtham Kumar <gowtham0794k@gmail.com>
2021-06-03 19:02:11 +0530

    multiple components.
    multiple components.

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Header extends React.Component {
+    render() {
+        return(
+        <div className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li>Home</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
